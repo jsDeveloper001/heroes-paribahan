a0fad20b6c0cddddd1f021df1fa33e33883bda84
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-02-19 23:22:09 +0600

    coupon code and price calculation implement done

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const totalTickets = document.getElementById("totalTickets");
 const ticketPrice = document.getElementById("ticketPrice");
 const totalPrice = document.getElementById("totalPrice");
 const grandTotal = document.getElementById("grandTotal");
+const couponElement = document.getElementById("couponElement");
+const couponButton = document.getElementById("couponButton");
+const couponField = document.getElementById("couponField");
+const couponCode1 = document.getElementById("couponCode1");
+const couponCode2 = document.getElementById("couponCode2");
+const discountAmount = document.getElementById("discountAmount");
+const confirmButton = document.getElementById("confirmButton");
+
+const phoneNumber = document.getElementById("phoneNumber");
+
+
 const totalTicketContainer = document.getElementById("totalTicketContainer");
 
 const bookedTickets = [];
@@ -12,6 +23,7 @@ function selectSeat(seat) {
       makeSelected(seat);
       updateAvailableSeats("totalSeats");
       createElementandUpdatePrice(seat);
+      calculateTicketPrice();
       bookedTickets.push(seat);
     } else {
       alert("This seat already booked");
@@ -24,7 +36,9 @@ function selectSeat(seat) {
 
 // set the background color for selected tickets
 function makeSelected(ticketId) {
-  return document.getElementById(ticketId).classList.add("bg-[#1DD101]");
+  return document
+    .getElementById(ticketId)
+    .classList.add("bg-[#1DD101]", "text-white");
 }
 
 // update the available ticket from the total tickets
@@ -53,3 +67,66 @@ function createElementandUpdatePrice(seat) {
     totalTicketContainer.appendChild(Price)
   );
 }
+
+// update the price of tickets
+function calculateTicketPrice() {
+  totalPrice.innerText =
+    parseFloat(totalPrice.innerText) + parseFloat(ticketPrice.innerText);
+  grandTotal.innerText =
+    parseFloat(grandTotal.innerText) + parseFloat(ticketPrice.innerText);
+}
+
+// apply coupon code if eligible
+function applyCoupon() {
+  if (
+    couponField.value === couponCode1.innerText ||
+    couponField.value === couponCode2.innerText
+  ) {
+    if (couponField.value === couponCode1.innerText) {
+      const discountPrice = parseFloat(totalPrice.innerText) * 0.15;
+
+      const h2 = document.createElement("h2");
+      h2.innerText = "Discount";
+      const p = document.createElement("p");
+      p.innerText = discountPrice;
+
+      discountAmount.appendChild(h2), discountAmount.appendChild(p);
+      grandTotal.innerText = parseFloat(grandTotal.innerText) - discountPrice;
+    } else {
+      const discountPrice = parseFloat(totalPrice.innerText) * 0.20;
+      const h2 = document.createElement("h2");
+      h2.innerText = "Discount";
+      const p = document.createElement("p");
+      p.innerText = discountPrice;
+
+      discountAmount.appendChild(h2), discountAmount.appendChild(p);
+      grandTotal.innerText = parseFloat(grandTotal.innerText) - discountPrice;
+    }
+    couponElement.classList.add("hidden");
+  } else {
+    alert("invalid coupon code");
+  }
+  couponField.value = "";
+}
+
+// enable or disable the coupon button
+couponField.addEventListener("click", function () {
+  if (bookedTickets.length == 4) {
+    couponButton.removeAttribute("disabled");
+  } else {
+    couponButton.setAttribute("disabled", true);
+    couponField.value = "";
+    alert("purchase 4 tickets to use coupon code");
+  }
+});
+
+// if condition is met then goto the confirm booking
+confirmButton.addEventListener("mousemove", function () {
+  if (bookedTickets.length >= 1 && phoneNumber.value) {
+    confirmButton.removeAttribute("disabled");
+  }
+  else {
+    alert("select minimum one seat and provide phone number")
+    confirmButton.setAttribute("disables", true)
+  }
+})
